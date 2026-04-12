8fa701ddf8d076e0dfd06901ece4546ba8afe24b
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-04-12 02:54:30 -0400

    Finish register page and added display names to the Model

--- HomeInsurance-MVC/HomeInsurance-MVC/Models/RegisterViewModel.cs
+++ HomeInsurance-MVC/HomeInsurance-MVC/Models/RegisterViewModel.cs
@@ -1,35 +1,14 @@
 ﻿/*
  * File: RegisterViewModel.cs
  * Project: HomeInsurance-MVC
- * Author(s): Mohammad
+ * Author(s): Mohammad, Julia
  * Date: [Enter Date]
  * Description:
  * This view model stores the input fields required to register a new user.
  * It is used to transfer registration data between the Register view
  * and the AccountController.
- */
-/*
- Purpose: Stores registration form input only.
-
-What it does
-
-This model is for the register page form.
-
-Usually includes:
-
-FullName
-Email
-Password
-ConfirmPassword
-PhoneNumber
-Why it exists
-
-The registration form needs fields that are not exactly the same as the stored User model.
-
-Example:
-
-ConfirmPassword is needed in the form
-but it should not be stored in the database
+ * Reference(s) :
+ * https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/controller-methods-views?view=aspnetcore-10.0
  */
 
 namespace HomeInsurance_MVC.Models
@@ -46,6 +25,7 @@ namespace HomeInsurance_MVC.Models
         /// Gets or sets the full name entered at registration.
         /// </summary>
         [Required]
+        [Display(Name = "Full Name *")]
         [StringLength(ValidationConstants.UserFullNameMaxLength)]
         public string FullName { get; set; } = string.Empty;
 
@@ -53,6 +33,7 @@ namespace HomeInsurance_MVC.Models
         /// Gets or sets the unique email entered by the user.
         /// </summary>
         [Required]
+        [Display(Name = "Email *")]
         [EmailAddress]
         [StringLength(ValidationConstants.UserEmailMaxLength)]
         public string Email { get; set; } = string.Empty;
@@ -61,6 +42,7 @@ namespace HomeInsurance_MVC.Models
         /// Gets or sets the password entered by the user.
         /// </summary>
         [Required]
+        [Display(Name = "Password *")]
         [DataType(DataType.Password)]
         [StringLength(ValidationConstants.PasswordMaxLength, MinimumLength = ValidationConstants.PasswordMinLength)]
         public string Password { get; set; } = string.Empty;
@@ -69,6 +51,7 @@ namespace HomeInsurance_MVC.Models
         /// Gets or sets the confirmation password entered by the user.
         /// </summary>
         [Required]
+        [Display(Name = "Confirm Password *")]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
         public string ConfirmPassword { get; set; } = string.Empty;
@@ -77,6 +60,7 @@ namespace HomeInsurance_MVC.Models
         /// Gets or sets the phone number entered by the user.
         /// </summary>
         [Phone]
+        [Display(Name = "Phone Number")]
         [StringLength(ValidationConstants.PhoneMaxLength)]
         public string? PhoneNumber { get; set; }
     }
